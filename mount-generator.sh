fde6081c19515ccce06cb2f91d3f357769575a4b
lesi123123 <34687493+lesi123123@users.noreply.github.com>
2017-12-19 17:40:21 +0100

    Delete mount-generator.sh

--- mount-generator.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-sudo apt-get -y install nfs-kernel-server
-sudo echo "Domain = 192.168.4.5" >>   /etc/idmapd.conf
-sudo echo "/home 192.168.4.5(rw,no_root_squash)" >>   /etc/idmapd.conf
-systemctl restart nfs-server
