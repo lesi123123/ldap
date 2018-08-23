bba498a26f34f9bee444c08f8b4ffe8d5399ae2f
lesi123123 <34687493+lesi123123@users.noreply.github.com>
2018-08-23 11:31:23 +0200

    Update mount-generator.sh

--- mount-generator.sh
+++ mount-generator.sh
@@ -2,4 +2,7 @@
 sudo apt-get -y install nfs-kernel-server
 sudo echo "Domain = 192.168.4.5" >>   /etc/idmapd.conf
 sudo echo "/home 192.168.4.5(rw,no_root_squash)" >>   /etc/idmapd.conf
+sudo echo "/home 192.168.4.5(rw,no_root_squash)" >>   /etc/exports
+
+
 systemctl restart nfs-server
